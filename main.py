fa38238a44564aeffefa898ccac2179e2f542104
Your Name <hai.phamcse@hcmut.edu.vn>
2022-08-21 10:41:04 +0700

    added temporary train function. Will improve when learned to use dataset

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ import model
 import transformers as trs
 import tensorflow as tf
 import numpy as np
+import tensorflow_datasets as tfds
+
+my_devices = tf.config.experimental.list_physical_devices(device_type='CPU')
+tf.config.experimental.set_visible_devices(devices= my_devices, device_type='CPU')
+
+# To find out which devices your operations and tensors are assigned to
+tf.debugging.set_log_device_placement(True)
 if __name__ == '__main__':
     print("MAIN")
     processor = preprocess.TextProcessing("vinai/phobert-base")
     processor.read_csv("data.csv")
 
-    m = model.QHD_PhoBert()
+    with tf.device('/cpu:0'):
+        m = model.QHD_PhoBert()
 
     train_encodings, val_encodings, train_labels, test_labels,train_dataset, val_dataset = processor.process()
-    print(m(train_encodings[0]))
+    #xxx_encodings: list of batch(transformers.tokenization_utils_base.BatchEncoding)
+    # for i in val_dataset.as_numpy_iterator():
+    #     for j in np.nditer(i[0]):
+    #         print(j)
+    #     print(i[1])
+    model.train(m, train_dataset)
--- model.py
+++ model.py
@@ -14,11 +14,17 @@ class QHD_PhoBert(tf.keras.Model):
 
     # a single Tensor with input_ids only and nothing else: model(inputs_ids)
     # a list of varying length with one or several input Tensors IN THE ORDER given in the docstring: model([input_ids, attention_mask]) or model([input_ids, attention_mask, token_type_ids])
-    def call(self, input_ids, **kwargs):
-        outputs = self.roberta(input_ids, **kwargs)
+    def call(self, inputs, **kwargs):
+        outputs = self.roberta(inputs, **kwargs)
         #MAY CAUSE ERROR WHEN INSERTING ENTIRE BATCH AS INPUTS
         #By transformers doc, the output[1] of Roberta layer is the padded output
         outputs = outputs[1]
         outputs = self.dense(outputs)
         outputs = self.classifier(outputs)
         return outputs
+
+
+def train(model, train_dataset):
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    model.compile(optimizer, loss=tf.keras.losses.MeanSquaredError())
+    model.fit(train_dataset.shuffle(1000).batch(16), epochs=2, batch_size=16)
