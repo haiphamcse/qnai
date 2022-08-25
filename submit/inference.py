45a94f0a85323a660ecd624c96494581e01933f2
Your Name <hai.phamcse@hcmut.edu.vn>
2022-08-25 14:16:15 +0700

    Still need to reshape numpy

---
+++ submit/inference.py
@@ -0,0 +1,7 @@
+from solver import ClassifyReviewSolver
+
+review_solver = ClassifyReviewSolver()
+
+sentence = ["Chào em"]
+
+print(review_solver.solve(sentence))
---
+++ train/eda.py
@@ -0,0 +1,4 @@
+from processing import TextProcessing
+
+if __name__ == '__main__':
+    print()
